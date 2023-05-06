c6ceb56c29b82df4ac2c3ec463979f7714afad0c
liujinhan <liujinhan@jingxunsoft.com>
2023-05-06 13:55:42 +0800

    day 7 ok

--- event_22/src/bin/ans_07.rs
+++ event_22/src/bin/ans_07.rs
@@ -1,74 +1,175 @@
 use std::collections::HashMap;
 
 #[derive(Debug)]
-enum FileType {
-    Dir,
-    File,
+struct Arena {
+    map: HashMap<usize, Item>,
+    cur: usize,
 }
 
 #[derive(Debug)]
-struct Node {
+struct Item {
     size: usize,
-    file_type: FileType,
+    is_dir: bool,
     name: String,
-    items: Vec<Node>,
+    parent: Option<usize>,
+    child: Vec<usize>,
 }
 
-impl Node {
+impl Arena {
     fn new() -> Self {
-        Node {
+        let mut map = HashMap::new();
+        let root = Item {
             size: 0,
-            file_type: FileType::Dir,
-            name: "/".into(),
-            items: Vec::new(),
+            is_dir: true,
+            name: "/".to_string(),
+            parent: None,
+            child: Vec::new(),
+        };
+        map.insert(0, root);
+        Arena { map, cur: 0 }
+    }
+
+    fn dir_size(&mut self) {
+        let mut size = 0;
+        let item = self.map.get(&self.cur).unwrap();
+
+        for id in &item.child {
+            let leafe = self.map.get(id).unwrap();
+            size += leafe.size;
+        }
+
+        let item = self.map.get_mut(&self.cur).unwrap();
+        item.size = size;
+    }
+
+    fn cd_dir(&mut self, name: &str) {
+        if name == ".." {
+            self.dir_size();
+        }
+        let item = self.map.get(&self.cur).unwrap();
+
+        if name == ".." {
+            if let Some(pre) = item.parent {
+                self.cur = pre;
+            }
+            return;
+        }
+
+        for id in &item.child {
+            let leafe = self.map.get(id).unwrap();
+            if leafe.is_dir && leafe.name == name {
+                self.cur = *id;
+                return;
+            }
         }
     }
 
     fn add_dir(&mut self, name: &str) {
-        self.items.push(Node {
+        // add new item to arena
+        let id = self.map.len(); // new id
+        let new_item = Item {
             size: 0,
-            file_type: FileType::Dir,
-            name: name.into(),
-            items: Vec::new(),
-        });
+            is_dir: true,
+            name: name.to_string(),
+            parent: Some(self.cur),
+            child: Vec::new(),
+        };
+        self.map.insert(id, new_item);
+
+        // add id to cur item
+        if let Some(item) = self.map.get_mut(&self.cur) {
+            item.child.push(id);
+        }
     }
 
     fn add_file(&mut self, name: &str, size: usize) {
-        self.items.push(Node {
+        // add new item to arena
+        let id = self.map.len(); // new id
+        let new_item = Item {
             size,
-            file_type: FileType::File,
-            name: name.into(),
-            items: Vec::new(),
-        });
-    }
-}
+            is_dir: false,
+            name: name.to_string(),
+            parent: Some(self.cur),
+            child: Vec::new(),
+        };
+        self.map.insert(id, new_item);
 
-struct Tree {
-    stack: Vec<Box<Node>>,
-    node: Node,
-    dirs: HashMap<String, usize>,
-}
-
-impl Tree {
-    fn new() -> Self {
-        Tree {
-            stack: vec![],
-            node: Node::new(),
+        // add id to cur item
+        if let Some(item) = self.map.get_mut(&self.cur) {
+            item.child.push(id);
         }
     }
+}
+
+fn cmd_parser(arena: &mut Arena, s: &str) {
+    // println!("{s}");
+    let mut iter = s.split_whitespace().skip(1);
+    let cmd = iter.next().unwrap();
+    if cmd == "ls" {
+        return;
+    }
+    let name = iter.next().unwrap();
+    // println!("{cmd} {dir}")
+    arena.cd_dir(name);
+}
+
+fn dir_parser(arena: &mut Arena, s: &str) {
+    let name = s.split_whitespace().skip(1).next().unwrap();
+    arena.add_dir(name);
+}
+
+fn file_parser(arena: &mut Arena, s: &str) {
+    let mut iter = s.split_whitespace();
+    let size = iter.next().unwrap().parse().unwrap();
+    let name = iter.next().unwrap();
+    arena.add_file(name, size);
+}
+
+fn go_to_root(arena: &mut Arena) {
+    while arena.cur > 0 {
+        arena.cd_dir("..");
+    }
+    arena.dir_size();
+}
+
+fn walk_a(arena: &Arena) -> usize {
+    arena
+        .map
+        .iter()
+        .filter(|&(_, item)| item.is_dir && item.size <= 100_000)
+        .map(|(_, item)| item.size)
+        .sum()
+}
+
+fn walk_b(arena: &Arena) -> usize {
+    let used_space = arena.map.get(&0).unwrap().size;
+    let left_space = 70_000_000 - used_space;
+    let need_free = 30_000_000 - left_space;
+    arena
+        .map
+        .iter()
+        .filter(|&(_, item)| item.is_dir && item.size >= need_free)
+        .map(|(_, item)| item.size)
+        .min()
+        .unwrap()
 }
 
 fn main() {
-    let mut node = Node::new();
-    let lines = include_str!("test.txt").lines();
-    for line in lines {
+    let mut arena = Arena::new();
+
+    let lines = include_str!("input_07.txt").lines();
+    for line in lines.skip(1) {
         // println!("{line}");
         if line.starts_with("$") {
-            println!("cmd---> {}", line);
+            cmd_parser(&mut arena, line);
         } else if line.starts_with("dir") {
-            println!("dir || {}", line);
+            dir_parser(&mut arena, line);
         } else {
-            println!("file ... {}", line);
+            file_parser(&mut arena, line);
         }
     }
+    go_to_root(&mut arena); // calc all dir size
+                            // println!("{:#?}", arena);
+    println!("part one: {}", walk_a(&arena));
+    println!("part two: {}", walk_b(&arena));
 }
